951a9afb170fb2dd82682662f0f3f9d93a0a5ec5
Jon Allen <jonathan.allen@boohoo.com>
2024-02-22 15:23:39 +0000

    Make it more readable

--- src/helpers/formatters.ts
+++ src/helpers/formatters.ts
@@ -8,14 +8,14 @@ export function toMs(value: string): number {
   const match = value.match(/(\d+)([a-z])?/);
   if (!match) return 0;
 
-  const total = Number(match[1]);
-  const type = match[2].toLowerCase();
+  let total = Number(match[1]);
+  const type = match[2]?.toLowerCase();
 
-  if (type === 's') return total * second;
-  if (type === 'm') return total * minute;
-  if (type === 'h') return total * hour;
-  if (type === 'd') return total * day;
-  if (type === 'w') return total * week;
+  if (type === 's') total *= second;
+  if (type === 'm') total *= minute;
+  if (type === 'h') total *= hour;
+  if (type === 'd') total *= day;
+  if (type === 'w') total *= week;
 
   return total;
 }
